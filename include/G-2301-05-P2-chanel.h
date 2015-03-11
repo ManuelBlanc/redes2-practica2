195d8e2f0d386140b7c93b802eb9aaf84d7b85ff
MBlanc <manuel.blanc@estudiante.uam.es>
2015-03-11 12:40:04 +0100

    Creado cabecera de channel y cambiado el nombre a util.h

--- include/G-2301-05-P2-chanel.h
+++ include/G-2301-05-P2-chanel.h
@@ -3,28 +3,28 @@
 
 #include "G-2301-05-P2-user.h"
 
-O - give "channel creator" status;
-o - give/take channel operator privilege;
-v - give/take the voice privilege;
+enum UserFlags {
+        USRFLAG_CREATOR    = (1<<0), // O - give "channel creator" status;
+        USRFLAG_OPERATOR   = (1<<1), // o - give/take channel operator privilege;
+        USRFLAG_VOICE      = (1<<2), // v - give/take the voice privilege;
+        USRFLAG_BAN        = (1<<3), // b - set/remove ban mask to keep users out;
+        USRFLAG_EXCEPTION  = (1<<4), // e - set/remove an exception mask to override a ban mask;
+        USRFLAG_INVITATION = (1<<5)  // I - set/remove an invitation mask to automatically override the invite-only flag;
+};
 
-a - toggle the anonymous channel flag;
-i - toggle the invite-only channel flag;
-m - toggle the moderated channel;
-n - toggle the no messages to channel from clients on the
-   outside;
-q - toggle the quiet channel flag;
-p - toggle the private channel flag;
-s - toggle the secret channel flag;
-r - toggle the server reop channel flag;
-t - toggle the topic settable by channel operator only flag;
-
-k - set/remove the channel key (password);
-l - set/remove the user limit to channel;
-
-b - set/remove ban mask to keep users out;
-e - set/remove an exception mask to override a ban mask;
-I - set/remove an invitation mask to automatically override
-   the invite-only flag;
+enum ChannelFlags {
+        FLAG_ANONYMOUS = (1<<0),  // a - toggle the anonymous channel flag;
+        FLAG_INVONLY   = (1<<1),  // i - toggle the invite-only channel flag;
+        FLAG_MODERATED = (1<<2),  // m - toggle the moderated channel;
+        FLAG_NOMSGS    = (1<<3),  // n - toggle the no messages to channel from clients on the outside;
+        FLAG_QUIET     = (1<<4),  // q - toggle the quiet channel flag;
+        FLAG_PRIVATE   = (1<<5),  // p - toggle the private channel flag;
+        FLAG_SECRET    = (1<<6),  // s - toggle the secret channel flag;
+        FLAG_REOP      = (1<<7),  // r - toggle the server reop channel flag;
+        FLAG_TOPIC     = (1<<8),  // t - toggle the topic settable by channel operator only flag;
+        FLAG_CHANKEY   = (1<<9),  // k - set/remove the channel key (password);
+        FLAG_USERLIMIT = (1<<10), // l - set/remove the user limit to channel;
+};
 
 struct UserChannel {
 	User*              	usr;
@@ -41,6 +41,30 @@ typedef struct Channel {
     Server*        	server;	/* Servidor al que pertenece 	*/
     struct Channel*	next;  	/* Puntero al siguiente canal	*/
 } Channel;
+
+Channel* channel_new(void);
+void channel_delete(Channel* chan);
+
+int channel_add_user(Channel* chan, User* usr);
+int channel_remove_user(Channel* chan, User* usr);
+int channel_get_flags_user(Channel* chan, User* usr, UserFlags* flags);
+int channel_set_flags_user(Channel* chan, User* usr, UserFlags flags, User* usr);
+int channel_unset_flags_user(Channel* chan, User* usr, UserFlags flags, User* usr);
+
+int channel_send_message(Channel* chan, User* usr, const char* msg);
+
+int channel_get_topic(Channel* chan, const char** topic);
+int channel_set_topic(Channel* chan, const char*  topic, User* usr);
+
+int channel_get_name(Channel* chan, const char** name);
+int channel_set_name(Channel* chan, const char*  name);
+
+int channel_get_passwd(Channel* chan, const char** passwd);
+int channel_set_passwd(Channel* chan, const char*  passwd);
+
+int channel_get_flags(Channel* chan, ChannelFlags* flags);
+int channel_set_flags(Channel* chan, ChannelFlags flags, User* usr);
+int channel_unset_flags(Channel* chan, ChannelFlags flags, User* usr);
 
 /*
 ** ==============================================
