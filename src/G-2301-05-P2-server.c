26c4a2158ca349a09a1d2670192a6b6b09e40cde
Ana <ana.alonsoa@estudiante.uam.es>
2015-04-04 21:42:00 +0200

    Arreglando para compilacion

--- src/G-2301-05-P2-server.c
+++ src/G-2301-05-P2-server.c
@@ -39,7 +39,6 @@ static void procesar_opciones(int argc, char * const *argv) {
 		{ "help",     	no_argument,      	NULL,      	'h'	},
 		{ "buffy",    	no_argument,      	NULL,      	'b'	},
 		{ "fluoride", 	required_argument,	NULL,      	'f'	},
-		{ "daggerset",	no_argument,      	&daggerset,	1  	},
 		{NULL,0,NULL,0}
 	};
         while (1) {
@@ -107,8 +106,7 @@ int server_accept(Server* serv){
 
 	int sock = accept(serv->sock, (struct sockaddr*) &user_addr, &usrlen);
 	User* user = user_new(serv, sock);
-	server_add_user(serv, user);
-	return OK;
+	return server_add_user(serv, user);
 }
 
 int server_is_nick_used(Server* serv, const char* nick) {
@@ -117,19 +115,30 @@ int server_is_nick_used(Server* serv, const char* nick) {
 }
 
 int server_add_user(Server* serv, User* user) {
-   	userlist_insert(serv->usrs, user);
+   	userlist_insert(serv->usrs, *user);
 	return OK;
 }
 
 int server_delete_user(Server* serv, const char* name) {
-	/*cde?????????????*/
-	User* usr = userlist_findByName(UserList list, const char* name);
+	UserList usr = userlist_findByName(serv->usrs, name);
+        if (usr == NULL) return ERR;
+        User usr2 = userlist_extract(usr);
+        user_delete(&usr2);
 	return OK;
 }
 
-int server_add_channel(Server* serv, const char* chan) {
-	/*ya sabemos que no esta repe?????????*/
-   	channellist_insert(serv->chan, chan);
+int server_add_channel(Server* serv, const char* name) {
+        ChannelList chan = channel_findByName(serv->chan, name);
+        if (chan != NULL) return ERR;
+   	channellist_insert(serv->chan, *channel_new(serv, name));
+	return OK;
+}
+
+int server_delete_channel(Server* serv, const char* name) {
+	ChannelList chan = channellist_findByName(serv->chan, name);
+        if (chan == NULL) return ERR;
+        Channel chan2 = channellist_extract(chan);
+        channel_delete(&chan2);
 	return OK;
 }
 
--- src/G-2301-05-P2-switches.c
+++ src/G-2301-05-P2-switches.c
@@ -64,164 +64,6 @@ int action_switch(User* usr, char* str) {
 	}
 }
 
-
-int action_switch(int what) {
-	switch (what) {
-		default: break;
-		case RPL_ADMINEMAIL     	: printf("%s\n", "Reply RPL_ADMINEMAIL recibido!"     	); break;
-		case RPL_ADMINLOC1      	: printf("%s\n", "Reply RPL_ADMINLOC1 recibido!"      	); break;
-		case RPL_ADMINLOC2      	: printf("%s\n", "Reply RPL_ADMINLOC2 recibido!"      	); break;
-		case RPL_ADMINME        	: printf("%s\n", "Reply RPL_ADMINME recibido!"        	); break;
-		case RPL_AWAY           	: printf("%s\n", "Reply RPL_AWAY recibido!"           	); break;
-		case RPL_BANLIST        	: printf("%s\n", "Reply RPL_BANLIST recibido!"        	); break;
-		case RPL_BOUNCE         	: printf("%s\n", "Reply RPL_BOUNCE recibido!"         	); break;
-		case RPL_CHANNELMODEIS  	: printf("%s\n", "Reply RPL_CHANNELMODEIS recibido!"  	); break;
-		case RPL_CHANNELURL     	: printf("%s\n", "Reply RPL_CHANNELURL recibido!"     	); break;
-		case RPL_CREATED        	: printf("%s\n", "Reply RPL_CREATED recibido!"        	); break;
-		case RPL_CREATIONTIME   	: printf("%s\n", "Reply RPL_CREATIONTIME recibido!"   	); break;
-		case RPL_ENDOFBANLIST   	: printf("%s\n", "Reply RPL_ENDOFBANLIST recibido!"   	); break;
-		case RPL_ENDOFEXCEPTLIST	: printf("%s\n", "Reply RPL_ENDOFEXCEPTLIST recibido!"	); break;
-		case RPL_ENDOFINFO      	: printf("%s\n", "Reply RPL_ENDOFINFO recibido!"      	); break;
-		case RPL_ENDOFINVITELIST	: printf("%s\n", "Reply RPL_ENDOFINVITELIST recibido!"	); break;
-		case RPL_ENDOFLINKS     	: printf("%s\n", "Reply RPL_ENDOFLINKS recibido!"     	); break;
-		case RPL_ENDOFMOTD      	: printf("%s\n", "Reply RPL_ENDOFMOTD recibido!"      	); break;
-		case RPL_ENDOFNAMES     	: printf("%s\n", "Reply RPL_ENDOFNAMES recibido!"     	); break;
-		case RPL_ENDOFSTATS     	: printf("%s\n", "Reply RPL_ENDOFSTATS recibido!"     	); break;
-		case RPL_ENDOFUSERS     	: printf("%s\n", "Reply RPL_ENDOFUSERS recibido!"     	); break;
-		case RPL_ENDOFWHO       	: printf("%s\n", "Reply RPL_ENDOFWHO recibido!"       	); break;
-		case RPL_ENDOFWHOIS     	: printf("%s\n", "Reply RPL_ENDOFWHOIS recibido!"     	); break;
-		case RPL_ENDOFWHOWAS    	: printf("%s\n", "Reply RPL_ENDOFWHOWAS recibido!"    	); break;
-		case RPL_EXCEPTLIST     	: printf("%s\n", "Reply RPL_EXCEPTLIST recibido!"     	); break;
-		case RPL_GLOBALUSERS    	: printf("%s\n", "Reply RPL_GLOBALUSERS recibido!"    	); break;
-		case RPL_INFO           	: printf("%s\n", "Reply RPL_INFO recibido!"           	); break;
-		case RPL_INVITELIST     	: printf("%s\n", "Reply RPL_INVITELIST recibido!"     	); break;
-		case RPL_INVITING       	: printf("%s\n", "Reply RPL_INVITING recibido!"       	); break;
-		case RPL_ISON           	: printf("%s\n", "Reply RPL_ISON recibido!"           	); break;
-		case RPL_LINKS          	: printf("%s\n", "Reply RPL_LINKS recibido!"          	); break;
-		case RPL_LIST           	: printf("%s\n", "Reply RPL_LIST recibido!"           	); break;
-		case RPL_LISTEND        	: printf("%s\n", "Reply RPL_LISTEND recibido!"        	); break;
-		case RPL_LISTSTART      	: printf("%s\n", "Reply RPL_LISTSTART recibido!"      	); break;
-		case RPL_LOCALUSERS     	: printf("%s\n", "Reply RPL_LOCALUSERS recibido!"     	); break;
-		case RPL_LUSERCHANNELS  	: printf("%s\n", "Reply RPL_LUSERCHANNELS recibido!"  	); break;
-		case RPL_LUSERCLIENT    	: printf("%s\n", "Reply RPL_LUSERCLIENT recibido!"    	); break;
-		case RPL_LUSERME        	: printf("%s\n", "Reply RPL_LUSERME recibido!"        	); break;
-		case RPL_LUSEROP        	: printf("%s\n", "Reply RPL_LUSEROP recibido!"        	); break;
-		case RPL_LUSERUNKNOWN   	: printf("%s\n", "Reply RPL_LUSERUNKNOWN recibido!"   	); break;
-		case RPL_MOTD           	: printf("%s\n", "Reply RPL_MOTD recibido!"           	); break;
-		case RPL_MOTDSTART      	: printf("%s\n", "Reply RPL_MOTDSTART recibido!"      	); break;
-		case RPL_MYINFO         	: printf("%s\n", "Reply RPL_MYINFO recibido!"         	); break;
-		case RPL_NAMREPLY       	: printf("%s\n", "Reply RPL_NAMREPLY recibido!"       	); break;
-		case RPL_NOTOPIC        	: printf("%s\n", "Reply RPL_NOTOPIC recibido!"        	); break;
-		case RPL_NOUSERS        	: printf("%s\n", "Reply RPL_NOUSERS recibido!"        	); break;
-		case RPL_NOWAWAY        	: printf("%s\n", "Reply RPL_NOWAWAY recibido!"        	); break;
-		case RPL_REHASHING      	: printf("%s\n", "Reply RPL_REHASHING recibido!"      	); break;
-		case RPL_SERVLIST       	: printf("%s\n", "Reply RPL_SERVLIST recibido!"       	); break;
-		case RPL_SERVLISTEND    	: printf("%s\n", "Reply RPL_SERVLISTEND recibido!"    	); break;
-		case RPL_STATSCOMMANDS  	: printf("%s\n", "Reply RPL_STATSCOMMANDS recibido!"  	); break;
-		case RPL_STATSLINKINFO  	: printf("%s\n", "Reply RPL_STATSLINKINFO recibido!"  	); break;
-		case RPL_STATSOLINE     	: printf("%s\n", "Reply RPL_STATSOLINE recibido!"     	); break;
-		case RPL_STATSUPTIME    	: printf("%s\n", "Reply RPL_STATSUPTIME recibido!"    	); break;
-		case RPL_SUMMONING      	: printf("%s\n", "Reply RPL_SUMMONING recibido!"      	); break;
-		case RPL_TIME           	: printf("%s\n", "Reply RPL_TIME recibido!"           	); break;
-		case RPL_TOPIC          	: printf("%s\n", "Reply RPL_TOPIC recibido!"          	); break;
-		case RPL_TOPICWHOTIME   	: printf("%s\n", "Reply RPL_TOPICWHOTIME recibido!"   	); break;
-		case RPL_TRACECLASS     	: printf("%s\n", "Reply RPL_TRACECLASS recibido!"     	); break;
-		case RPL_TRACECONNECT   	: printf("%s\n", "Reply RPL_TRACECONNECT recibido!"   	); break;
-		case RPL_TRACECONNECTING	: printf("%s\n", "Reply RPL_TRACECONNECTING recibido!"	); break;
-		case RPL_TRACEEND       	: printf("%s\n", "Reply RPL_TRACEEND recibido!"       	); break;
-		case RPL_TRACEHANDSHAKE 	: printf("%s\n", "Reply RPL_TRACEHANDSHAKE recibido!" 	); break;
-		case RPL_TRACELINK      	: printf("%s\n", "Reply RPL_TRACELINK recibido!"      	); break;
-		case RPL_TRACELOG       	: printf("%s\n", "Reply RPL_TRACELOG recibido!"       	); break;
-		case RPL_TRACENEWTYPE   	: printf("%s\n", "Reply RPL_TRACENEWTYPE recibido!"   	); break;
-		case RPL_TRACEOPERATOR  	: printf("%s\n", "Reply RPL_TRACEOPERATOR recibido!"  	); break;
-		case RPL_TRACESERVER    	: printf("%s\n", "Reply RPL_TRACESERVER recibido!"    	); break;
-		case RPL_TRACESERVICE   	: printf("%s\n", "Reply RPL_TRACESERVICE recibido!"   	); break;
-		case RPL_TRACEUNKNOWN   	: printf("%s\n", "Reply RPL_TRACEUNKNOWN recibido!"   	); break;
-		case RPL_TRACEUSER      	: printf("%s\n", "Reply RPL_TRACEUSER recibido!"      	); break;
-		case RPL_TRYAGAIN       	: printf("%s\n", "Reply RPL_TRYAGAIN recibido!"       	); break;
-		case RPL_UMODEIS        	: printf("%s\n", "Reply RPL_UMODEIS recibido!"        	); break;
-		case RPL_UNAWAY         	: printf("%s\n", "Reply RPL_UNAWAY recibido!"         	); break;
-		case RPL_UNIQOPIS       	: printf("%s\n", "Reply RPL_UNIQOPIS recibido!"       	); break;
-		case RPL_USERHOST       	: printf("%s\n", "Reply RPL_USERHOST recibido!"       	); break;
-		case RPL_USERS          	: printf("%s\n", "Reply RPL_USERS recibido!"          	); break;
-		case RPL_USERSSTART     	: printf("%s\n", "Reply RPL_USERSSTART recibido!"     	); break;
-		case RPL_VERSION        	: printf("%s\n", "Reply RPL_VERSION recibido!"        	); break;
-		case RPL_WELCOME        	: printf("%s\n", "Reply RPL_WELCOME recibido!"        	); break;
-		case RPL_WHOISCHANNELS  	: printf("%s\n", "Reply RPL_WHOISCHANNELS recibido!"  	); break;
-		case RPL_WHOISIDLE      	: printf("%s\n", "Reply RPL_WHOISIDLE recibido!"      	); break;
-		case RPL_WHOISOPERATOR  	: printf("%s\n", "Reply RPL_WHOISOPERATOR recibido!"  	); break;
-		case RPL_WHOISSERVER    	: printf("%s\n", "Reply RPL_WHOISSERVER recibido!"    	); break;
-		case RPL_WHOISUSER      	: printf("%s\n", "Reply RPL_WHOISUSER recibido!"      	); break;
-		case RPL_WHOREPLY       	: printf("%s\n", "Reply RPL_WHOREPLY recibido!"       	); break;
-		case RPL_WHOWASUSER     	: printf("%s\n", "Reply RPL_WHOWASUSER recibido!"     	); break;
-		case RPL_YOUREOPER      	: printf("%s\n", "Reply RPL_YOUREOPER recibido!"      	); break;
-		case RPL_YOURESERVICE   	: printf("%s\n", "Reply RPL_YOURESERVICE recibido!"   	); break;
-		case RPL_YOURHOST       	: printf("%s\n", "Reply RPL_YOURHOST recibido!"       	); break;
-		case RPL_YOURID         	: printf("%s\n", "Reply RPL_YOURID recibido!"         	); break;
-	}
-}
-
-
-int action_switch(int what) {
-	switch (what) {
-		default: break;
-		case ERR_ALREADYREGISTRED 	: printf("%s\n", "ERR_ALREADYREGISTRED" 	); break;
-		case ERR_BADCHANMASK      	: printf("%s\n", "ERR_BADCHANMASK"      	); break;
-		case ERR_BADCHANNELKEY    	: printf("%s\n", "ERR_BADCHANNELKEY"    	); break;
-		case ERR_BADMASK          	: printf("%s\n", "ERR_BADMASK"          	); break;
-		case ERR_BANLISTFULL      	: printf("%s\n", "ERR_BANLISTFULL"      	); break;
-		case ERR_BANNEDFROMCHAN   	: printf("%s\n", "ERR_BANNEDFROMCHAN"   	); break;
-		case ERR_CANNOTSENDTOCHAN 	: printf("%s\n", "ERR_CANNOTSENDTOCHAN" 	); break;
-		case ERR_CANTKILLSERVER   	: printf("%s\n", "ERR_CANTKILLSERVER"   	); break;
-		case ERR_CHANNELISFULL    	: printf("%s\n", "ERR_CHANNELISFULL"    	); break;
-		case ERR_CHANOPRIVSNEEDED 	: printf("%s\n", "ERR_CHANOPRIVSNEEDED" 	); break;
-		case ERR_ERRONEUSNICKNAME 	: printf("%s\n", "ERR_ERRONEUSNICKNAME" 	); break;
-		case ERR_FILEERROR        	: printf("%s\n", "ERR_FILEERROR"        	); break;
-		case ERR_INVITEONLYCHAN   	: printf("%s\n", "ERR_INVITEONLYCHAN"   	); break;
-		case ERR_KEYSET           	: printf("%s\n", "ERR_KEYSET"           	); break;
-		case ERR_NEEDMOREPARAMS   	: printf("%s\n", "ERR_NEEDMOREPARAMS"   	); break;
-		case ERR_NICKCOLLISION    	: printf("%s\n", "ERR_NICKCOLLISION"    	); break;
-		case ERR_NICKNAMEINUSE    	: printf("%s\n", "ERR_NICKNAMEINUSE"    	); break;
-		case ERR_NOADMININFO      	: printf("%s\n", "ERR_NOADMININFO"      	); break;
-		case ERR_NOCHANMODES      	: printf("%s\n", "ERR_NOCHANMODES"      	); break;
-		case ERR_NOLOGIN          	: printf("%s\n", "ERR_NOLOGIN"          	); break;
-		case ERR_NOMOTD           	: printf("%s\n", "ERR_NOMOTD"           	); break;
-		case ERR_NONICKNAMEGIVEN  	: printf("%s\n", "ERR_NONICKNAMEGIVEN"  	); break;
-		case ERR_NOOPERHOST       	: printf("%s\n", "ERR_NOOPERHOST"       	); break;
-		case ERR_NOORIGIN         	: printf("%s\n", "ERR_NOORIGIN"         	); break;
-		case ERR_NOPERMFORHOST    	: printf("%s\n", "ERR_NOPERMFORHOST"    	); break;
-		case ERR_NOPRIVILEGES     	: printf("%s\n", "ERR_NOPRIVILEGES"     	); break;
-		case ERR_NORECIPIENT      	: printf("%s\n", "ERR_NORECIPIENT"      	); break;
-		case ERR_NOSUCHCHANNEL    	: printf("%s\n", "ERR_NOSUCHCHANNEL"    	); break;
-		case ERR_NOSUCHNICK       	: printf("%s\n", "ERR_NOSUCHNICK"       	); break;
-		case ERR_NOSUCHSERVER     	: printf("%s\n", "ERR_NOSUCHSERVER"     	); break;
-		case ERR_NOSUCHSERVICE    	: printf("%s\n", "ERR_NOSUCHSERVICE"    	); break;
-		case ERR_NOTEXTTOSEND     	: printf("%s\n", "ERR_NOTEXTTOSEND"     	); break;
-		case ERR_NOTONCHANNEL     	: printf("%s\n", "ERR_NOTONCHANNEL"     	); break;
-		case ERR_NOTOPLEVEL       	: printf("%s\n", "ERR_NOTOPLEVEL"       	); break;
-		case ERR_NOTREGISTERED    	: printf("%s\n", "ERR_NOTREGISTERED"    	); break;
-		case ERR_PASSWDMISMATCH   	: printf("%s\n", "ERR_PASSWDMISMATCH"   	); break;
-		case ERR_RESTRICTED       	: printf("%s\n", "ERR_RESTRICTED"       	); break;
-		case ERR_SUMMONDISABLED   	: printf("%s\n", "ERR_SUMMONDISABLED"   	); break;
-		case ERR_TOOMANYCHANNELS  	: printf("%s\n", "ERR_TOOMANYCHANNELS"  	); break;
-		case ERR_TOOMANYMATCHES   	: printf("%s\n", "ERR_TOOMANYMATCHES"   	); break;
-		case ERR_TOOMANYTARGETS   	: printf("%s\n", "ERR_TOOMANYTARGETS"   	); break;
-		case ERR_UMODEUNKNOWNFLAG 	: printf("%s\n", "ERR_UMODEUNKNOWNFLAG" 	); break;
-		case ERR_UNAVAILRESOURCE  	: printf("%s\n", "ERR_UNAVAILRESOURCE"  	); break;
-		case ERR_UNIQOPPRIVSNEEDED	: printf("%s\n", "ERR_UNIQOPPRIVSNEEDED"	); break;
-		case ERR_UNKNOWNCOMMAND   	: printf("%s\n", "ERR_UNKNOWNCOMMAND"   	); break;
-		case ERR_UNKNOWNMODE      	: printf("%s\n", "ERR_UNKNOWNMODE"      	); break;
-		case ERR_USERNOTINCHANNEL 	: printf("%s\n", "ERR_USERNOTINCHANNEL" 	); break;
-		case ERR_USERONCHANNEL    	: printf("%s\n", "ERR_USERONCHANNEL"    	); break;
-		case ERR_USERSDISABLED    	: printf("%s\n", "ERR_USERSDISABLED"    	); break;
-		case ERR_USERSDONTMATCH   	: printf("%s\n", "ERR_USERSDONTMATCH"   	); break;
-		case ERR_WASNOSUCHNICK    	: printf("%s\n", "ERR_WASNOSUCHNICK"    	); break;
-		case ERR_WILDTOPLEVEL     	: printf("%s\n", "ERR_WILDTOPLEVEL"     	); break;
-		case ERR_YOUREBANNEDCREEP 	: printf("%s\n", "ERR_YOUREBANNEDCREEP" 	); break;
-		case ERR_YOUWILLBEBANNED  	: printf("%s\n", "ERR_YOUWILLBEBANNED"  	); break;
-	}
-}
-
 int serverrcv_cmd(User* usr, char* str){
 	char* cmd;
 	int more_commands = 1;
@@ -401,6 +243,25 @@ int serverrcv_mode(Server* serv, User* usr, const char* str) {
 
 	}
 	return OK;*/
+}
+
+int serverrcv_user(Server* serv, User* usr, const char* str) {
+	/*long ret;
+	//Command: USER
+	//Parameters: <user> <mode> <unused> <realname>
+	if(0 != IRCParse_User()){
+		IRC_ErrNeedMoreParams();
+		user_send_cmd(usr, buf);
+		return ERR;
+	}
+	User* usr = userlist_findByName();
+	if(NULL == usr){
+		setters...
+		server_add_user();
+		return OK;
+	}
+	IRC_ErrAlreadyRegistred();
+	return ERR;*/
 }
 
 int serverrcv_quit(Server* serv, User* usr, const char* str) {
