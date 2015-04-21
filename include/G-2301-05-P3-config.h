10885bd29a1b8bc25319c9321fabfa9a6c6c398c
Ana <ana.alonsoa@estudiante.uam.es>
2015-04-21 23:51:26 +0200

    Nombres de fichero con P3 en vez de P2

---
+++ include/G-2301-05-P3-config.h
@@ -0,0 +1,101 @@
+/* include/G-2301-05-P2-config.h.  Generated from G-2301-05-P2-config.h.in by configure.  */
+/* include/G-2301-05-P2-config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Maximum channel key length */
+#define CHANNEL_MAX_KEY_LEN 50
+
+/* Maximum channel name length */
+#define CHANNEL_MAX_NAME_LEN 50
+
+/* Maximum channel topic length */
+#define CHANNEL_MAX_TOPIC_LEN 50
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <redes2/irc.h> header file. */
+/* #undef HAVE_REDES2_IRC_H */
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Maximum command length */
+#define IRC_MAX_CMD_LEN 512
+
+/* Name of package */
+#define PACKAGE "redes2-server"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "manuel.blanc@estudiante.uam.es"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "redes2-server"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "redes2-server 0.1.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "redes2-server"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.1.0"
+
+/* Maximum server name length */
+#define SERVER_MAX_NAME_LEN 50
+
+/* Maximum info disconnections saved */
+#define SERVER_MAX_OUT 50
+
+/* Maximum connections */
+#define SERVER_MAX_USERS 150
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Maximum away message length */
+#define USER_MAX_AWAY_LEN 50
+
+/* Maximum host name length */
+#define USER_MAX_HOST_LEN 64
+
+/* Maximum username length */
+#define USER_MAX_NAME_LEN 9
+
+/* Maximum user length */
+#define USER_MAX_NICK_LEN 9
+
+/* Maximum user prefix length */
+#define USER_MAX_PRE_LEN 83
+
+/* Maximum user real name length */
+#define USER_MAX_RNAME_LEN 50
+
+/* Version number of package */
+
+/* Define to 1 if `lex' declares `yytext' as a `char *' by default, not a
+   `char[]'. */
+/* #undef YYTEXT_POINTER */
