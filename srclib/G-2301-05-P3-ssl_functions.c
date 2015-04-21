1b9b7776eb7dc5d29778d149d006ac1ea65be5e5
MBlanc <manuel.blanc@estudiante.uam.es>
2015-04-22 00:50:19 +0200

    Implementado las 4 ultimas funciones de ssl_functions

--- srclib/G-2301-05-P3-ssl_functions.c
+++ srclib/G-2301-05-P3-ssl_functions.c
@@ -98,3 +98,44 @@ int prueba(void) {
 
 	SSL_CTX_set_verify(r2ssl->ctx, 0, 0);
 }
+
+
+
+/** Comprueba que el certificado del par es valida segun un CA */
+int evaluar_post_connectar_SSL(Redes2_SSL* r2ssl) {
+	if (NULL == r2ssl) return ERR;
+
+	// Obtenemos el certificado del par
+	X509* cert = SSL_get_peer_certificate(r2ssl->ssl);
+
+	// Si no se ha recibido, abortamos
+	if (NULL == cert) return ERR;
+
+	// Ya no lo necesitamos y lo liberamos
+	X509_free(cert);
+
+	// Si ha ocurrido un error al verificar, abortamos tambien
+	if (X509_V_OK != SSL_get_verify_result(r2ssl->ssl)) return ERR;
+
+	// Si hemos llegado hasta aqui, exito!!
+	return OK;
+}
+
+/** Envia datos por la conexion encapsulada, como send() */
+ssize_t enviar_datos_SSL(Redes2_SSL* r2ssl, void* buf, size_t len) {
+	if (NULL == r2ssl) return -1;
+	return (ssize_t)SSL_write(r2ssl->ssl, buf, (int)len);
+}
+
+
+/** Envia datos por la conexion encapsulada, como recv() */
+ssize_t recibir_datos_SSL(Redes2_SSL* r2ssl, void* buf, size_t len) {
+	if (NULL == r2ssl) return -1;
+	return (ssize_t)SSL_read(r2ssl->ssl, buf, (int)len);
+}
+
+/** Destruye la conexion */
+int cerrar_canal_SSL(Redes2_SSL* r2ssl) {
+	SSL_shutdown(r2ssl->ssl);
+	SSL_free(r2ssl->ssl);
+}
